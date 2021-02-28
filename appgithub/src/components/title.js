3cd75b3033f64cc9d5ca577918aa27393d12f147
Cacio de Castro <cacio.castro@gmail.com>
2021-02-28 11:32:05 -0300

    conslusão da estilização

--- appgithub/src/components/title.js
+++ appgithub/src/components/title.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const Title = ()=>{
-    return(
-        <div className="title">
-            <img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" />
-        <h1>Pesquisa de usuários GitHub</h1>
-        </div>
-    )
-}
+const Title = () => {
+  return (
+    <div className="title">
+      <img src="https://github.githubassets.com/images/modules/logos_page/Octocat.png" />
+      <h1>Pesquisa de usuários GitHub</h1>
+    </div>
+  );
+};
 
 export default Title;
--- appgithub/src/components/userInfo.js
+++ appgithub/src/components/userInfo.js
@@ -1,24 +1,24 @@
-'use strict';
+"use strict";
 
-import React, { PropTypes } from 'react';
+import React, { PropTypes } from "react";
 
 const UserInfo = ({ userInfo }) => (
   <div className="user-info">
     <div className="user-card">
-    <img src={userInfo.image} />
+      <img src={userInfo.image} />
 
-    <h1 className="user-name">
-      <a href={`https://api.github.com/user/${userInfo.login}`}>
-        {userInfo.username}
-      </a>
-    </h1>
+      <h1 className="user-name">
+        <a href={`https://api.github.com/user/${userInfo.login}`}>
+          {userInfo.username}
+        </a>
+      </h1>
     </div>
-<div className="card-info">
-    <ul className="repos-info">
-      <li>Repositórios: {userInfo.repos}</li>
-      <li>Seguidores: {userInfo.followers}</li>
-      <li>Seguindo: {userInfo.following}</li>
-    </ul>
+    <div className="card-info">
+      <ul className="repos-info">
+        <li>Repositórios: {userInfo.repos}</li>
+        <li>Seguidores: {userInfo.followers}</li>
+        <li>Seguindo: {userInfo.following}</li>
+      </ul>
     </div>
   </div>
 );
